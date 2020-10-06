3af5119f411c638ac3ed0474808c0c448d9a9562
Ivan Plesskih <terma95@gmail.com>
2020-10-06 21:10:32 +0500

    Fixed agent waiting

--- packages/agent/index.js
+++ packages/agent/index.js
@@ -1,10 +1,17 @@
 export async function start(app, swUrl, scope='/') {
     const u = new URL(swUrl, origin)
     u.searchParams.set('app', app)
-    const reg = await navigator.serviceWorker.register(u.toString(), {scope})
-    await reg.update()
-    if (!navigator.serviceWorker.controller || reg.installing || reg.waiting)
-        await new Promise(ok => navigator.serviceWorker.addEventListener('controllerchange', ok))
+    const prevController = navigator.serviceWorker.controller
+    await navigator.serviceWorker.register(u.toString(), {scope}).then(reg => reg.update())
+    const reg = await navigator.serviceWorker.getRegistration()
+    if (!prevController || reg.installing || reg.waiting || (reg.active !== prevController)) {
+        await new Promise(ok => navigator.serviceWorker.addEventListener('controllerchange', () => {
+            if (navigator.serviceWorker.controller.state === 'activating')
+                navigator.serviceWorker.controller.addEventListener('statechange', ok)
+            else
+                ok()
+        }))
+    }
     const es = new EventSource('/__mprt__')
     addEventListener('beforeunload', () => reg.active?.postMessage({type: 'unload'}))
     await new Promise(ok => es.onopen = ok)
